d2afcc38058ac82a31d2b5004dd5262c30279e67
Vaibhav Sisodiya <91018916+Vaib215@users.noreply.github.com>
2023-09-20 20:21:54 +0000

    Enabled video, remove clerk logo

--- app/meeting/page.tsx
+++ app/meeting/page.tsx
@@ -1,30 +1,24 @@
 "use client";
 
-import CustomLeave from '@/components/layout/custom-leave';
-import {
-  LiveKitRoom,
-  RoomAudioRenderer,
-  VideoConference
-} from '@livekit/components-react';
+import LiveKitBare from '@/components/livekit/livekit-bare';
+import { LiveKitRoom } from '@livekit/components-react';
 import '@livekit/components-styles';
 import { useSearchParams } from 'next/navigation';
 
 export default function Page() {
   const searchParams = useSearchParams();
-  
+
   return (
     <LiveKitRoom
       video={true}
       audio={true}
       token={searchParams.get('token')!}
-      connectOptions={{ autoSubscribe: false }}
+      connectOptions={{ autoSubscribe: true }}
       serverUrl={process.env.NEXT_PUBLIC_LIVEKIT_URL}
       data-lk-theme="default"
       style={{ height: '100dvh' }}
     >
-      <VideoConference />
-      <RoomAudioRenderer />
-      <CustomLeave />
+      <LiveKitBare />
     </LiveKitRoom>
   );
 }
