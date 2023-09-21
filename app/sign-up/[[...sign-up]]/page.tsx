fdc962b8205a69938e734270562e0dddab8c3f3c
uniquesonu <21bcs10550@cuchd.in>
2023-09-21 10:38:16 +0530

    Added metadata to pages

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,4 +1,9 @@
 import { SignUp } from "@clerk/nextjs";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Sign in",
+};
 
 export default function Page() {
   return (
